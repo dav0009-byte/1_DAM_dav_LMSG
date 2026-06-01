10753965ac8ba592e8752118ef01bd0f5cdab37a
dav0009-byte <dav0009@alu.medac.es>
2026-06-01 18:34:12 +0200

    hyh

--- JavaScript/Ejercicio19/js/script.js
+++ JavaScript/Ejercicio19/js/script.js
@@ -4,9 +4,14 @@ function adduser() {
     var contrasenia = document.getElementById("password");
     var confirmarContrasenia = document.getElementById("confirmPassword");
 
+    // 1. Verificar que los campos no estén vacíos
+
     if(usuario.value == "" || contrasenia.value =="" || confirmarContrasenia.value == ""){
 
         alert("Falta algun dato");
+
+
+    // 2. Verificar que las contraseñas coincidan
 
     } else if (contrasenia.value != confirmarContrasenia.value) {
 
@@ -19,12 +24,14 @@ function adduser() {
         contrasenia.type = "text";
         confirmarContrasenia.type = "text";
 
+    // 3. Si todo es correcto, mostrar el usuario y contraseña en la sección de usuarios registrados
+
     } else { 
 
         var userinfo = document.getElementById("registeredUsers");
         var parrafo = document.createElement("p");
 
-        parrafo.innerHTML = "Usuario: " + usuario.value + "// Contraseña : " + contrasenia.value;
+        parrafo.innerHTML = "Usuario: " + usuario.value + "  //   Contraseña : " + contrasenia.value;
         userinfo.appendChild(parrafo);
 
         document.getElementById("username").value="";
